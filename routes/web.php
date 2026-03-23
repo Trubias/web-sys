d7c5a81348c535cd5f5320116725eec7f0fdd8d7
Trubias <jay.trubias@urios.edu.ph>
2026-03-23 21:32:23 +0800

    kalas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/run-migrations', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return "Migrations ran successfully! <br><br> Output: <br>" . \Illuminate\Support\Facades\Artisan::output();
+    } catch (\Exception $e) {
+        return "Migration error: " . $e->getMessage();
+    }
+});
+
+Route::get('/run-seed', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+        return "Seeding ran successfully! <br><br> Output: <br>" . \Illuminate\Support\Facades\Artisan::output();
+    } catch (\Exception $e) {
+        return "Seeding error: " . $e->getMessage();
+    }
+});
+
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
